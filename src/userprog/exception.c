2f8381ac6b21281dea6e83b093eb93fa923868e1
xehoth <824547322@qq.com>
2021-10-06 17:01:46 +0800

    init p3

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -150,16 +150,18 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
   //   !BEGIN MODIFY
-  if (not_present || (is_kernel_vaddr (fault_addr) && user))
+  /* Invalid address */
+  if (!fault_addr || !not_present || (is_kernel_vaddr (fault_addr) && user))
     {
       syscall_exit (-1);
     }
   //  !END MODIFY
 
+
+  /* To implement virtual memory, delete the rest of the function
+     body, and replace it with code that brings in the page to
+     which fault_addr refers. */
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
